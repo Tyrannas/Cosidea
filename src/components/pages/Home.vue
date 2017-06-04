f1232a9ba760bf432985f6fdcd04a08d57f88029
Félix <seomeonefefe@gmail.com>
2017-06-04 13:23:30 +0200

    structure

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="test">
+    <div class="messageBox">{{lorem}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      lorem: 'Lorem ipsum sit dolor.'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
