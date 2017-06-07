2cd975848f93fa99c7d17b5659ae1bb59348e380
Félix <seomeonefefe@gmail.com>
2017-06-07 12:51:15 +0200

    changes

--- front/src/components/pages/Recif.vue
+++ front/src/components/pages/Recif.vue
@@ -85,7 +85,8 @@ export default {
                 }
                 this.$refs.sigma.addNode({
                     title: 'n' + i,
-                    tags: tags
+                    tags: tags,
+                    id: Math.random()
                 });
             }
         },
